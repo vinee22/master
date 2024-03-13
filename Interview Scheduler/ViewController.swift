183db76907f74586e611a8353bab071513715c61
mpkumarasamy <113576733+mpkumarasamy@users.noreply.github.com>
2024-03-13 19:01:16 +0530

    Initial commit

---
+++ Interview Scheduler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interview Scheduler
+//
+//  Created by HS on 13/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
