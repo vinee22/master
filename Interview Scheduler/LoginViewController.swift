79421e75edaf5bc9c22826a7ae0b1aee3ca70b7e
vinee22 <vineethve3@gmail.com>
2024-03-14 10:56:38 +0530

    second commit

--- Interview Scheduler/LoginViewController.swift
+++ Interview Scheduler/LoginViewController.swift
@@ -6,3 +6,178 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    var validationLabel = UILabel() // Declare validationLabel outside of viewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Background Image
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "BackgroundImage")
+        backgroundImage.contentMode = .scaleAspectFill
+        self.view.addSubview(backgroundImage)
+        
+        // Load the original images
+        guard let backImage = UIImage(named: "BackIcon") else { return }
+        guard let homeImage = UIImage(named: "HomeIcon") else { return }
+        
+        // Resize the back icon image
+        let resizedBackImage = backImage.resized(to: CGSize(width: 50, height: 50))
+        
+        // Resize the home icon image
+        let resizedHomeImage = homeImage.resized(to: CGSize(width: 50, height: 50))
+        
+        // Tint the resized images
+        let tintedBackImage = resizedBackImage.withTintColor(AppSettings().primaryColor(), renderingMode: .alwaysOriginal)
+        let tintedHomeImage = resizedHomeImage.withTintColor(AppSettings().primaryColor(), renderingMode: .alwaysOriginal)
+        
+        
+        // Navigation Bar
+        let backButton = UIBarButtonItem(image: tintedBackImage, style: .plain, target: self, action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = backButton
+        
+        // Navigation Home
+        let homeButton = UIBarButtonItem(image: tintedHomeImage, style: .plain, target: self, action: #selector(homeButtonTapped))
+        navigationItem.rightBarButtonItem = homeButton
+        
+        title = AppSettings().signUpScreenTitle()
+        
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: AppSettings().primaryColor(),
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30) // Adjust font size as needed
+        ]
+        
+        // Email Text Field
+        let emailTextField = UITextField()
+        emailTextField.placeholder = AppSettings().loginEmailPlaceholderText()
+        emailTextField.tintColor = AppSettings().secondaryColor()
+        emailTextField.textColor = AppSettings().secondaryColor()
+        emailTextField.borderStyle = .roundedRect
+        emailTextField.layer.borderColor = AppSettings().backgroundColor().cgColor
+        emailTextField.layer.borderWidth = 1
+        emailTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(emailTextField)
+        
+        // Add target to monitor changes in the emailTextField
+        emailTextField.addTarget(self, action: #selector(validateEmail(_:)), for: .editingChanged)
+        
+        // Password Text Field
+        let passwordTextField = UITextField()
+        passwordTextField.placeholder = AppSettings().loginPasswordPlaceholderText()
+        passwordTextField.tintColor = AppSettings().secondaryColor()
+        passwordTextField.textColor = AppSettings().secondaryColor()
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.layer.borderColor = AppSettings().backgroundColor().cgColor
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(passwordTextField)
+        
+        // Add target to monitor changes in the passwordTextField
+        passwordTextField.addTarget(self, action: #selector(validatePassword(_:)), for: .editingChanged)
+        
+        // Login Button
+        let loginButton = UIButton()
+        loginButton.setTitle(AppSettings().loginButtonText(), for: .normal)
+        loginButton.backgroundColor = AppSettings().primaryColor()
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        self.view.addSubview(loginButton)
+        
+        // Don't have an account Label
+        let dontHaveAccountLabel = UILabel()
+        dontHaveAccountLabel.text = AppSettings().loginDontHaveAnAccount()
+        dontHaveAccountLabel.textColor = AppSettings().secondaryColor()
+        dontHaveAccountLabel.backgroundColor = AppSettings().primaryColor()
+        dontHaveAccountLabel.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dontHaveAccountLabelTapped))
+        dontHaveAccountLabel.addGestureRecognizer(tapGesture)
+        self.view.addSubview(dontHaveAccountLabel)
+        
+        // Layout
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        dontHaveAccountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100), // Adjust the constant as needed
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 10),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            dontHaveAccountLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            dontHaveAccountLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+        
+        
+    }
+    
+    @objc func backButtonTapped() {
+        // Handle back button action
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func homeButtonTapped() {
+        // Handle home button action
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc func validateEmail(_ textField: UITextField) {
+        guard let email = textField.text else { return }
+        
+        // Perform email validation using a regular expression
+        let emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        let isEmailValid = emailPredicate.evaluate(with: email)
+        
+        // Update validation message label visibility based on the validation result
+        if isEmailValid {
+            textField.layer.borderColor = UIColor.green.cgColor
+        } else {
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    
+    
+    @objc func validatePassword(_ textField: UITextField) {
+        guard let password = textField.text else { return }
+        
+        // Perform password validation
+        let passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@*$#]).{8,}$"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        let isPasswordValid = passwordPredicate.evaluate(with: password)
+        
+        // Update validation message label visibility based on the validation result
+        
+        if isPasswordValid {
+            textField.layer.borderColor = UIColor.green.cgColor
+        } else {
+            // Password does not meet the criteria
+            // For example, change the border color to red to indicate validation failure
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    
+    @objc func loginButtonTapped() {
+        // Handle login button action
+        print("Login button tapped")
+    }
+    
+    @objc func dontHaveAccountLabelTapped() {
+        // Navigate to another view controller for new account registration
+        let registerViewController = SignUpViewController()
+        navigationController?.pushViewController(registerViewController, animated: true)
+    }
+}
--- Interview Scheduler/SignUpViewController.swift
+++ Interview Scheduler/SignUpViewController.swift
@@ -6,3 +6,333 @@
 //
 
 import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+    
+    let dobTextField = UITextField()
+    var passwordTextField = UITextField()
+    var firstNameTextField = UITextField()
+    var lastNameTextField = UITextField()
+    var datePicker = UIDatePicker()
+    var emailTextField = UITextField()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Background Image
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "BackgroundImage")
+        backgroundImage.contentMode = .scaleAspectFill
+        self.view.addSubview(backgroundImage)
+        
+        // White Background View
+        let whiteBackgroundView = UIView()
+        whiteBackgroundView.backgroundColor = .white
+        self.view.addSubview(whiteBackgroundView)
+        self.view.sendSubviewToBack(whiteBackgroundView)
+        
+        // Load the original images
+        guard let backImage = UIImage(named: "BackIcon") else { return }
+        guard let homeImage = UIImage(named: "HomeIcon") else { return }
+        
+        // Resize the back icon image
+        let resizedBackImage = backImage.resized(to: CGSize(width: 50, height: 50))
+        
+        // Resize the home icon image
+        let resizedHomeImage = homeImage.resized(to: CGSize(width: 50, height: 50))
+        
+        // Tint the resized images
+        let tintedBackImage = resizedBackImage.withTintColor(AppSettings().primaryColor(), renderingMode: .alwaysOriginal)
+        let tintedHomeImage = resizedHomeImage.withTintColor(AppSettings().primaryColor(), renderingMode: .alwaysOriginal)
+        
+        
+        // Navigation Bar
+        let backButton = UIBarButtonItem(image: tintedBackImage, style: .plain, target: self, action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = backButton
+        
+        // Navigation Home
+        let homeButton = UIBarButtonItem(image: tintedHomeImage, style: .plain, target: self, action: #selector(homeButtonTapped))
+        navigationItem.rightBarButtonItem = homeButton
+        
+        title = AppSettings().signUpScreenTitle()
+        
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: AppSettings().primaryColor(),
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30) // Adjust font size as needed
+        ]
+        
+        // First Name Text Field
+        firstNameTextField.placeholder = AppSettings().signupFirstNamePlaceholderText()
+        firstNameTextField.borderStyle = .roundedRect
+        firstNameTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(firstNameTextField)
+        
+        // Last Name Text Field
+        
+        lastNameTextField.placeholder = AppSettings().signupLastNamePlaceholderText()
+        lastNameTextField.borderStyle = .roundedRect
+        lastNameTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(lastNameTextField)
+        
+        // Date of Birth Text Field
+        dobTextField.placeholder = AppSettings().signupDateofBirthPlaceholderText()
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        dobTextField.inputView = datePicker
+        dobTextField.borderStyle = .roundedRect
+        dobTextField.backgroundColor = AppSettings().highContrastColor()
+        dobTextField.delegate = self // Set the delegate to self
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
+        toolbar.setItems([flexibleSpace, doneButton], animated: false)
+        
+        dobTextField.inputAccessoryView = toolbar
+        self.view.addSubview(dobTextField)
+        
+        // Email Text Field
+        emailTextField.placeholder = AppSettings().loginEmailPlaceholderText()
+        emailTextField.tintColor = AppSettings().secondaryColor()
+        emailTextField.textColor = AppSettings().secondaryColor()
+        emailTextField.borderStyle = .roundedRect
+        emailTextField.layer.borderColor = AppSettings().backgroundColor().cgColor
+        emailTextField.layer.borderWidth = 1
+        emailTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(emailTextField)
+        
+        emailTextField.addTarget(self, action: #selector(validateEmail(_:)), for: .editingChanged)
+        
+        
+        // Password Text Field
+        passwordTextField = UITextField()
+        passwordTextField.placeholder = AppSettings().signupPasswordPlaceholderText()
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.layer.borderColor = AppSettings().backgroundColor().cgColor
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(passwordTextField)
+        
+        passwordTextField.addTarget(self, action: #selector(validatePassword(_:)), for: .editingChanged)
+        
+        
+        // Confirm Password Text Field
+        let confirmPasswordTextField = UITextField()
+        confirmPasswordTextField.placeholder = AppSettings().signupConfirmPasswordPlaceholderText()
+        confirmPasswordTextField.isSecureTextEntry = true
+        confirmPasswordTextField.borderStyle = .roundedRect
+        confirmPasswordTextField.layer.borderColor = AppSettings().backgroundColor().cgColor
+        confirmPasswordTextField.layer.borderWidth = 1
+        confirmPasswordTextField.backgroundColor = AppSettings().highContrastColor()
+        self.view.addSubview(confirmPasswordTextField)
+        
+        confirmPasswordTextField.addTarget(self, action: #selector(validateConfirmPassword(_:)), for: .editingChanged)
+        
+        
+        // Sign Up Button
+        let signUpButton = UIButton()
+        signUpButton.setTitle(AppSettings().signUpButtonText(), for: .normal)
+        signUpButton.backgroundColor = AppSettings().primaryColor()
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        self.view.addSubview(signUpButton)
+        
+        // Already have an account Label
+        let alreadyHaveAccountLabel = UILabel()
+        alreadyHaveAccountLabel.text = AppSettings().signupAlreadyHaveAnAccountText()
+        alreadyHaveAccountLabel.textColor = AppSettings().secondaryColor()
+        alreadyHaveAccountLabel.backgroundColor = AppSettings().primaryColor()
+        alreadyHaveAccountLabel.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(alreadyHaveAccountLabelTapped))
+        alreadyHaveAccountLabel.addGestureRecognizer(tapGesture)
+        self.view.addSubview(alreadyHaveAccountLabel)
+        
+        
+        // Layout
+        firstNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        lastNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        dobTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        confirmPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        alreadyHaveAccountLabel.translatesAutoresizingMaskIntoConstraints = false
+        whiteBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            firstNameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            firstNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            firstNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            lastNameTextField.topAnchor.constraint(equalTo: firstNameTextField.bottomAnchor, constant: 10),
+            lastNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            lastNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            dobTextField.topAnchor.constraint(equalTo: lastNameTextField.bottomAnchor, constant: 10),
+            dobTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            dobTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            emailTextField.topAnchor.constraint(equalTo: dobTextField.bottomAnchor, constant: 10),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 10),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            confirmPasswordTextField.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 10),
+            confirmPasswordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            confirmPasswordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            signUpButton.topAnchor.constraint(equalTo: confirmPasswordTextField.bottomAnchor, constant: 20),
+            signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            signUpButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            whiteBackgroundView.topAnchor.constraint(equalTo: firstNameTextField.topAnchor, constant: -10),
+            whiteBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            whiteBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            whiteBackgroundView.bottomAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: 10),
+            
+            alreadyHaveAccountLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            alreadyHaveAccountLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == dobTextField {
+            let datePicker = textField.inputView as! UIDatePicker
+            // Handle the case where the dobTextField is double-clicked to open the date picker
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd/MM/yyyy"
+            textField.text = dateFormatter.string(from: datePicker.date)
+            datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+        }
+    }
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker) {
+        // Update the dobTextField with the selected date from the date picker
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        dobTextField.text = dateFormatter.string(from: sender.date)
+    }
+    
+    @objc func backButtonTapped() {
+        // Handle back button action
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func homeButtonTapped() {
+        // Handle home button action
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc func validateEmail(_ textField: UITextField) {
+        guard let email = textField.text else { return }
+        
+        // Perform email validation using a regular expression
+        let emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        let isEmailValid = emailPredicate.evaluate(with: email)
+        
+        // Update validation message label visibility based on the validation result
+        if isEmailValid {
+            textField.layer.borderColor = UIColor.green.cgColor
+        } else {
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    
+    @objc func validatePassword(_ textField: UITextField) {
+        guard let password = textField.text else { return }
+        
+        // Perform password validation
+        let passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@*$#]).{8,}$"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        let isPasswordValid = passwordPredicate.evaluate(with: password)
+        
+        // Update validation message label visibility based on the validation result
+        
+        if isPasswordValid {
+            textField.layer.borderColor = UIColor.green.cgColor
+        } else {
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    
+    @objc func validateConfirmPassword(_ textField: UITextField) {
+        guard let confirmPassword = textField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        // Check if confirmPassword matches the password
+        let isPasswordMatch = confirmPassword == password
+        
+        // Update validation message label visibility based on the validation result
+        if isPasswordMatch {
+            textField.layer.borderColor = UIColor.green.cgColor
+        } else {
+            textField.layer.borderColor = UIColor.red.cgColor
+        }
+    }
+    
+    @objc func signUpButtonTapped() {
+        // Validate user input
+        guard let firstName = firstNameTextField.text, !firstName.isEmpty,
+              let lastName = lastNameTextField.text, !lastName.isEmpty,
+              let dateOfBirth = dobTextField.text, !dateOfBirth.isEmpty,
+              let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            // Handle incomplete or invalid input
+            return
+        }
+        
+        // Create a User object
+        let user = User(firstName: firstName, lastName: lastName, dateOfBirth: dateOfBirth, email: email, password: password)
+        print("SignupDetails:\(user)")
+        // Save user to the database
+        if saveUserToDatabase(user) {
+            // Successfully saved user, do any additional tasks like showing success message, navigating to another screen, etc.
+            print("User saved successfully")
+        } else {
+            // Failed to save user, handle the error appropriately
+            print("Failed to save user")
+        }
+    }
+    
+    func saveUserToDatabase(_ user: User) -> Bool {
+        
+        return true // Placeholder return value
+    }
+    
+    
+    @objc func alreadyHaveAccountLabelTapped() {
+        // Navigate to another view controller for existing users
+        let loginViewController = LoginViewController()
+        navigationController?.pushViewController(loginViewController, animated: true)
+    }
+    
+    @objc func doneButtonTapped() {
+        // Dismiss the keyboard or hide the date picker when the "Done" button is tapped
+        dobTextField.resignFirstResponder()
+    }
+    
+    struct User {
+        let firstName: String
+        let lastName: String
+        let dateOfBirth: String
+        let email: String
+        let password: String
+    }
+}
+
+extension UIImage {
+    func resized(to newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: newSize))
+        return UIGraphicsGetImageFromCurrentImageContext() ?? self
+    }
+}
